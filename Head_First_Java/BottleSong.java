2213b57065d4325ca9dc22ecf454de801412fa2c
codewithash <402308182@my.richfield.ac.za>
2023-11-05 18:10:54 +0200

    Bottle song

---
+++ Head_First_Java/BottleSong.java
@@ -0,0 +1,27 @@
+package Head_First_Java;
+
+public class BottleSong {
+    
+    public static void main(String[] args) {
+        int bottlesNum = 10;
+        String word = "bottles";
+
+        while (bottlesNum > 0) {
+            
+            if (bottlesNum == 1) {
+                word = "bottle";
+            }
+
+            System.out.println(bottlesNum + " green " + word + ", hanging on the wall ");
+            System.out.println(bottlesNum + " green " + word + ", hanging on the wall ");
+            System.out.println("And if one bottle should accidentally fall");
+            bottlesNum--;
+
+            if (bottlesNum > 0) {
+                System.out.println("There'll be " + bottlesNum + " green " + word + ", hanging on the wall");
+            } else {
+                System.out.println("There'll be no green bottles, hanging on the wall");
+            }
+        }
+    }
+}
