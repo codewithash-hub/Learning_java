772be6384dfe1e571843c6e7273e690f2e70d20a
codewithash <402308182@my.richfield.ac.za>
2023-11-05 19:04:35 +0200

    Shuffle1

---
+++ Head_First_Java/Shuffle1.java
@@ -0,0 +1,31 @@
+package Head_First_Java;
+
+public class Shuffle1 {
+    public static void main(String[] args) {
+        int x = 3;
+
+
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+                x = x - 1;
+                System.out.print("-");
+            }
+
+
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+
+            
+            if (x == 2) {
+                System.out.print("b c");
+                x = x - 1;
+                System.out.print("-");
+            }
+            
+        }
+
+    }
+}
