b1e5c42810542262431b5d8e997e340c12996bb0
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2025-03-11 15:35:09 +0200

    StreamingGuess

---
+++ StreamingSong/StreamingSongTestDrive.java
@@ -0,0 +1,10 @@
+public class StreamingSongTestDrive {
+    public static void main(String[] args) {
+        StreamingSong song = new StreamingSong();
+
+        song.artist = "The Beatles";
+        song.title = "Come Together";
+        song.play();
+        song.printDetails();
+    }
+}
